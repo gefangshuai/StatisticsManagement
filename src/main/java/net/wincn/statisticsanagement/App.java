d1a9157bc99dbf1ebb8b324193746e838ac30100
gefangshuai <gefangshuai@163.com>
2014-03-06 21:21:31 +0800

    init project

---
+++ src/main/java/net/wincn/statisticsanagement/App.java
@@ -0,0 +1,13 @@
+package net.wincn.statisticsanagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
